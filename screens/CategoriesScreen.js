e4d37eb7cfb6020341c055ceb7ca19b95e150457
Silas Boutwell <silaslee.b@gmail.com>
2025-03-10 13:42:32 -0700

    added navigation to the app

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,13 +2,18 @@ import { CATEGORIES } from '../data/dummy-data';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-function renderCategoryItem(itemData) {
-  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
-}
 
-function CategoriesScreen() {
+function CategoriesScreen({navigation}) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview', {categoryId: itemData.item.id});
+    } 
+  
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />;
+  }
+
   return (
-    <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+    <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2} />
   )
 }
 
